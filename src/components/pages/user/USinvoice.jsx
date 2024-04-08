4b77fe5e04822c72939111bde9cf7bdf7b6f73ce
AekJ4s <101738507+AekJ4s@users.noreply.github.com>
2024-04-08 17:56:16 +0700

    Edit InvoiceOfUser

--- src/components/pages/user/USinvoice.jsx
+++ src/components/pages/user/USinvoice.jsx
@@ -1,10 +1,19 @@
-import Selectbar from '../../../assets/selectbar'
 import { styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import Grid from '@mui/material/Grid'
-import MyComponent from '../../../assets/date'
-import { orderDetail } from '../../../data'
+import { productList } from '../../../data'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import FormGroup from '@mui/material/FormGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import {
+  TableColumnReordering,
+  TableFixedColumns,
+} from '@devexpress/dx-react-grid'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,122 +28,226 @@ export default function USinvoice() {
     <div className=" position-relative mt-5 ">
       <h3 className=" m-3">
         <span>We Live</span>
-        <span className=" text-success ms-2">| ชำระสินค้า</span>
+        <span className=" text-success ms-2">| รายการสินค้า</span>
       </h3>
-      <div className="m-2">
-        {orderDetail.map((detail, i) => {
-          // if (detail.from.fbName == 'Wittaya R.')
-          // ใช้ในการกำหนด ให้ชื่อ user ตรงกับออเดอร์เท่านั้นถึงจะโชว์ให้จ่ายเงิน
-          return (
-            <div
-              key={detail.id}
-              className="card card-header mb-3 bg-warning bg-opacity-75 "
-            >
-              <Box sx={{ flexGrow: 1 }} className="m-3">
-                <Grid container spacing={3} key={detail.id}>
-                  <Grid item xs={2}>
-                    <Item className="bg-primary text-white">
-                      <h5>ออเดอร์ # {detail.from.orderID}</h5>
-                    </Item>
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Item className=" bg-info-subtle text-center">
-                      <h5 className="text-danger">
-                        Facebook name :
-                        "เพิ่มเงื่อนไขส่วนนี้ให้แสดงเฉพาะที่ตรงกับชื่อ User
-                        ที่ล็อคอินเข้ามา
-                      </h5>
-                    </Item>
-                  </Grid>
-                  <Grid item xs={5}>
-                    <Item className=" bg-info-subtle text-center">
-                      <MyComponent />
-                    </Item>
-                  </Grid>
-                </Grid>
-              </Box>
-
-              <div className="card card-body">
-                <table className="table">
-                  <thead className="table-success">
-                    <tr>
-                      <th className="text-center">#</th>
-                      <th className="text-center">รายการ</th>
-                      <th className="text-center">จำนวน</th>
-                      <th className="text-center">ราคา</th>
-                    </tr>
-                  </thead>
-                  <tbody className=" table-striped">
-                    <tr key={detail.id}>
-                      <th className="text-center">{i + 1}</th>
-                      <th className="text-center">
-                        {detail.from.itemId} | {detail.from.itemNm}
-                      </th>
-                      <th className="text-center">{detail.from.among} ชิ้น</th>
-                      <th className="text-center">{detail.from.itempr} บาท </th>
-                    </tr>
-                    <tr>
-                      <th className="text-center">{}</th>
-                      <th className="text-center">ส่วนลด</th>
-                      <th className="text-center">{}</th>
-                      <th className="text-center">
-                        {detail.from.discount} บาท
-                      </th>
-                    </tr>
-                    <tr>
-                      <th className="text-center">{}</th>
-                      <th className="text-center">
-                        <Selectbar />
-                      </th>
-                      <th className="text-center">{}</th>
-                      <th className="text-center text-danger">
-                        รอดึงราคาจาก expressdetail
-                      </th>
-                    </tr>
-                    <tr>
-                      <th className="text-center">{}</th>
-                      <th className="text-center">รวม</th>
-                      <th className="text-center">{detail.from.among} ชิ้น</th>
-                      <th className="text-center text-danger">
-                        รอ ฟังก์ชั่นในการรวมราคา
-                      </th>
-                    </tr>
-                  </tbody>
-                </table>
-
-                <div>
-                  <h5>
-                    <span className="text-success">เลือก </span> | วันเวลาที่โอน
-                  </h5>
-                  <input type="date" className=" w-25 float-start" />
-                  <input type="time" className=" w-25 ms-3" />
-                </div>
-                <div className="mt-3">
-                  <h5>จำนวนเงิน</h5>
-                  <input
-                    type="number"
-                    className=" w-25"
-                    placeholder="กรอกตัวเลขเท่านั้น"
-                  ></input>
-                </div>
-                <div className="mt-3">
-                  <h5>อัพโหลดภาพสลิป</h5>
-                  <input type="file"></input>
-                </div>
-              </div>
-              <div className="text-center mt-2">
-                <button className="btn btn-sm btn-primary">
-                  ส่งแบบฟอร์มชำระเงิน
-                </button>
-                <button className="btn btn-sm btn-danger ms-3">
-                  ยกเลิกคำสั่งซื้อนี้
-                </button>
-              </div>
-            </div>
-          )
-        })}
+      <div className="card">
+        <div className=" text-center">
+          <br />
+          <span> Order :</span>
+          <span className=" text-danger">#12160</span> <br />
+          <span> เลขที่บัญชี....</span>
+          <br />
+          เลขที่บัญชี
+          <br />
+          <br />
+          --------------------------------------------
+          <br />
+          <br />
+          พร้อมเพย์ xxxxxxxxxx
+          <br />
+          KBANK xxx-x-xxxxx-x
+          <br />
+          ชื่อบัญชี นายเจษฎากร คุ้มเดช
+          <br />
+          <br />
+          🙏 รบกวนขอความกรุณาลูกค้า 💢 โอนยอดบิลต่อบิลนะคะ
+          แล้วค่อยเอฟใหม่ได้คะ💢
+          <br /> 💢หากมียอดค้างหักลบยอดเอง โอนได้เลยคะ
+          รบกวนแนบรูปยอดค้างไว้ได้เลยคะ ขอบคุณมากค่ะ🙏
+          <br /> 🙏 ถ้าสินค้ามีตำหนิกรุณารีบแจ้ง รับเปลี่ยน
+          หรือคืนสินค้ามีตำหนิจากร้าน ส่งผิดสีผิดแบบ ผิดไซส์ เท่านั้นคะ
+          ขอบพระคุณมากคะ🙏
+          <br />
+          <br />
+        </div>
       </div>
-      <div className="text-center"></div>
+      <div className=" mt-3">
+        <div className=" card">
+          <div className=" m-3">
+            <span>Order</span>
+            <span className=" text-danger ms-3">#12160</span>
+            <span className="ms-3">Facebook Name : ..............</span>
+            <br />
+            <span>วันที่ทำรายการ :</span>
+            <span className="ms-3">.................</span>
+          </div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell className="text-success text-center">#</TableCell>
+                  <TableCell className="text-success text-center">
+                    CF CODE
+                  </TableCell>
+                  <TableCell className="text-success text-center">
+                    NAME
+                  </TableCell>
+                  <TableCell className="text-success text-center">
+                    AMOUNT
+                  </TableCell>
+                  <TableCell className="text-success text-center">
+                    PRICE
+                  </TableCell>
+                  <TableCell className="text-success text-center">
+                    SUMMARY
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {productList.map((product, i) => {
+                  return (
+                    <TableRow
+                      key={product.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell className="text-center">{i + 1}</TableCell>
+                      <TableCell className="text-center">
+                        {product.from.CFcode}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {product.from.proName}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {product.from.proAmount}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {product.from.proPrice}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {product.from.proSum}
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+                {/* Additional row for discount, shipping cost, and total */}
+                <TableRow>
+                  <TableCell colSpan={1} />
+                  <TableCell className="text-center">ส่วนลด</TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center">0</TableCell>
+                </TableRow>
+                {/* Additional row for displaying discount, shipping cost, and total */}
+                <TableRow>
+                  <TableCell colSpan={1} />
+                  <TableCell className="text-center">ส่วนลด</TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center">49</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1} />
+                  <TableCell className="text-center">รวม (....) ชิ้น</TableCell>
+                  <TableCell className="text-center">ราคา ...... บาท</TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center"></TableCell>
+                  <TableCell className="text-center">49</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <div className=" m-3 ms-5 border-3 border-top border-bottom border-success">
+            <form>
+              <div className=" container float-start ">
+                <div className="row mt-3">
+                  <label>
+                    {' '}
+                    วันที่ทำรายการ
+                    <br />
+                    <input type="date" className="w-25" />
+                    <input type="time" className="w-25 ms-3" />
+                  </label>
+                </div>
+                <div className="row mt-3">
+                  <label>
+                    จำนวนเงินที่ทำรายการ
+                    <br />
+                    <input type=" number" className=" text-body" />
+                  </label>
+
+                  <label className=" mt-3">
+                    อัพโหลดภาพสลิปการโอนเงิน
+                    <br />
+                    <input
+                      type="file"
+                      className=" img-thumbnail bg-success-subtle"
+                    ></input>
+                  </label>
+                </div>
+                <div className=" row mt-3">
+                  <label>
+                    ชื่อ
+                    <br />
+                    <input
+                      type=" text "
+                      placeholder="ใส่ชื่อผู้โอน"
+                      className=" w-100"
+                    />
+                  </label>
+                </div>
+                <div className=" row mt-3">
+                  <label>
+                    ที่อยู่
+                    <br />
+                    <textarea
+                      type=" text "
+                      placeholder="กรอกที่อยู่ของลูกค้า ระบุให้ชัดเจน"
+                      className="w-100"
+                    />
+                  </label>
+                </div>
+
+                <div className=" row mt-3 w-100">
+                  <div className="col">
+                    <label>
+                      ตำบล/แขวง
+                      <br />
+                      <input className="w-auto" type=" text " />
+                    </label>
+                  </div>
+                  <div className="col">
+                    <label>
+                      อำเภอ/เขต
+                      <br />
+                      <input className="w-auto" type=" text " />
+                    </label>
+                  </div>
+                </div>
+                <div className=" row mt-3 w-100">
+                  <div className="col">
+                    <label>
+                      จังหวัด
+                      <br />
+                      <input className="w-auto" type=" text " />
+                    </label>
+                  </div>
+                  <div className="col">
+                    <label>
+                      รหัสไปรษณีย์
+                      <br />
+                      <input className="w-auto" type=" text " />
+                    </label>
+                  </div>
+
+                  <div className=" row mt-3">
+                    <label>
+                      โทรศัพท์
+                      <br />
+                      <input type=" text " className=" w-100" />
+                    </label>
+                  </div>
+                </div>
+                <button className="btn btn-primary mt-3 mb-3 " type="submit">
+                  บันทึก
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
