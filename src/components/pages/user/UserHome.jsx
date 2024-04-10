dcb43f24f260f883d483c6b8c4cce5d2b059b6f9
jayrboy <j.jayox_@hotmail.com>
2024-04-10 16:17:13 +0700

    update page order

--- src/components/pages/user/UserHome.jsx
+++ src/components/pages/user/UserHome.jsx
@@ -8,19 +8,15 @@ export default function UserHome() {
 
   return (
     <React.Fragment>
-      {user.length === 0 ? (
-        <div
-          className="card mt-5 mx-auto p-4 rounded"
-          style={{ width: '400px', background: '#fff' }}
-        >
-          <h1>
-            WE Live App <span className="badge bg-secondary">New</span>
-          </h1>
-          <p>Home Page User</p>
-        </div>
-      ) : (
-        <UserOrder />
-      )}
+      <div
+        className="card mt-5 mx-auto p-4 rounded"
+        style={{ width: '400px', background: '#fff' }}
+      >
+        <h1>
+          WE Live App <span className="badge bg-secondary">New</span>
+        </h1>
+        <p>Home Page User</p>
+      </div>
     </React.Fragment>
   )
 }
