4a88dbf4279f9f3d8d9323e5b2267a205600325e
jarboy <j.jayox_@hotmail.com>
2024-09-28 20:33:57 +0700

    changed url

--- src/App.jsx
+++ src/App.jsx
@@ -56,8 +56,8 @@ import TermOfServices from './views/TermOfServices'
 import Profile from './views/Profile'
 
 // export const baseURL = 'https://vercel-server-weliveapp.vercel.app'
-// export const baseURL = 'https://api-weliveapp.azurewebsites.net'
-export const baseURL = 'http://localhost:8000'
+export const baseURL = 'https://api-weliveapp.azurewebsites.net'
+// export const baseURL = 'http://localhost:8000'
 
 function App() {
   const navigate = useNavigate()
