12cc9066f9cc9ebc9fb30027708985aa185f2089
jarboy <j.jayox_@hotmail.com>
2024-10-05 06:10:21 +0700

    complete express

--- src/views/express/ExpressCreate.jsx
+++ src/views/express/ExpressCreate.jsx
@@ -1,7 +1,17 @@
 import { baseURL } from '../../App'
-import { useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useState, useRef } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+import {
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Button,
+  TextField,
+  FormHelperText,
+} from '@mui/material'
 
 import CloseIcon from '@mui/icons-material/Close'
 
@@ -9,10 +19,61 @@ export default function ExpressCreate() {
   const form = useRef()
   const navigate = useNavigate()
 
+  const [selectedExpress, setSelectedExpress] = useState('')
+  const [selectedUrl, setSelectedUrl] = useState('')
+  const [errors, setErrors] = useState({})
+
+  const expressList = [
+    {
+      value: 'ไปรษณีย์ไทย',
+      url: 'https://track.thailandpost.co.th/',
+    },
+    {
+      value: 'KERRY',
+      url: 'https://th.kerryexpress.com/th/track/?spm=a2o7e.lsc-im.0.0.2c6628f5ii8zFd&track=KERPU070489109',
+    },
+    {
+      value: 'FLASH',
+      url: 'https://www.flashexpress.co.th/fle/tracking?se=TH20015CSHZ95E',
+    },
+    {
+      value: 'J&T',
+      url: 'https://www.jtexpress.co.th/service/track',
+    },
+    { value: 'DHL', url: 'https://www.dhl.com/th-th/home/tracking.html' },
+  ]
+
+  const handleSelectChange = (event) => {
+    const selectedValue = event.target.value
+    const selectedExpressItem = expressList.find(
+      (express) => express.value === selectedValue
+    )
+    setSelectedExpress(selectedValue)
+    setSelectedUrl(selectedExpressItem?.url || '')
+    setErrors((prev) => ({ ...prev, selectedExpress: '' })) // clear error
+  }
+
+  const validateForm = (formData) => {
+    let tempErrors = {}
+    if (!selectedExpress) tempErrors.selectedExpress = 'กรุณาเลือกชื่อขนส่ง'
+    if (!formData.get('fprice')) tempErrors.fprice = 'กรุณากรอกค่าส่งเริ่มต้น'
+    return tempErrors
+  }
+
   const onSubmitForm = (event) => {
     event.preventDefault()
     const formData = new FormData(form.current)
+    formData.append('exname', selectedExpress)
+    formData.append('expressUrl', selectedUrl)
+
+    const validationErrors = validateForm(formData)
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors)
+      return
+    }
+
     const formEnt = Object.fromEntries(formData.entries())
+    console.log(formEnt)
 
     fetch(`${baseURL}/api/ex/create`, {
       method: 'POST',
@@ -23,13 +84,13 @@ export default function ExpressCreate() {
       .then((result) => {
         if (result === 'true') {
           form.current.reset()
-          alert('ข้อมูลถูกจัดเก็บแล้ว')
+          toast.success('ข้อมูลถูกจัดเก็บแล้ว')
         } else {
-          alert('เกิดข้อผิดพลาด ข้อมูลไม่ถูกบันทึก')
+          toast.error('เกิดข้อผิดพลาด ข้อมูลไม่ถูกบันทึก')
         }
         navigate('/express')
       })
-      .catch((e) => alert(e))
+      .catch((e) => toast.error(e))
   }
 
   return (
@@ -45,71 +106,89 @@ export default function ExpressCreate() {
           </button>
         </span>
         <form onSubmit={onSubmitForm} ref={form} className="p-4">
-          <label className="form-label">ชื่อขนส่ง</label>
-          <input
-            type="text"
-            name="exname"
-            placeholder="' J&T Express '"
-            className="form-control form-control-sm"
-            required
-          />
+          <FormControl
+            fullWidth
+            margin="normal"
+            error={!!errors.selectedExpress}
+          >
+            <InputLabel id="express-name-select-label">ชื่อขนส่ง</InputLabel>
+            <Select
+              labelId="express-name-select-label"
+              label="ชื่อขนส่ง"
+              id="express-name-select"
+              value={selectedExpress}
+              onChange={handleSelectChange}
+            >
+              {expressList.map((express) => (
+                <MenuItem key={express.value} value={express.value}>
+                  {express.value}
+                </MenuItem>
+              ))}
+            </Select>
+            {errors.selectedExpress && (
+              <FormHelperText>{errors.selectedExpress}</FormHelperText>
+            )}
+          </FormControl>
 
-          <label className="form-label mt-2">ค่าส่งเริ่มต้น</label>
-          <input
-            type="number"
+          <TextField
             name="fprice"
-            placeholder="' 50 ฿ '"
-            className="form-control form-control-sm"
-            required
+            label="ค่าส่งเริ่มต้น"
+            type="number"
+            fullWidth
+            margin="normal"
+            error={!!errors.fprice}
+            helperText={errors.fprice}
           />
 
-          <label className="form-label mt-2">ค่าส่งชิ้นต่อไป</label>
-          <input
-            type="number"
+          {/* <TextField
             name="sprice"
-            placeholder="' 30  '"
-            className="form-control form-control-sm"
+            label="ค่าส่งชิ้นต่อไป"
+            type="number"
+            fullWidth
+            margin="normal"
           />
 
-          <label className="form-label mt-2">ค่าส่งสูงสุด</label>
-          <input
-            type="number"
+          <TextField
             name="maxprice"
-            placeholder="' 100  '"
-            className="form-control form-control-sm"
-          />
-
-          <label className="form-label mt-2">ส่งฟรีต่อเมื่อยอดถึง</label>
-          <input
+            label="ค่าส่งสูงสุด"
             type="number"
+            fullWidth
+            margin="normal"
+          />
+
+          <TextField
             name="whenfprice"
-            placeholder="' 1000 '"
-            className="form-control form-control-sm"
+            label="ส่งฟรีต่อเมื่อยอดถึง"
+            type="number"
+            fullWidth
+            margin="normal"
           />
 
-          <label className="form-label mt-2">ค่าส่งเพิ่มเติมกรณี CODE</label>
-          <input
+          <TextField
+            name="selectcod"
+            label="ค่าส่งเพิ่มเติมกรณี CODE"
             type="text"
-            name="name"
-            placeholder="' 30 '"
-            className="form-control form-control-sm"
-          />
+            fullWidth
+            margin="normal"
+          /> */}
 
-          <label className="form-label mt-2">วันที่เริ่มต้น</label>
-          <input
-            type="Date"
+          <TextField
             name="date_start"
-            className="form-control form-control-sm mb-3"
+            label="วันที่เริ่มต้น"
+            type="date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            fullWidth
+            margin="normal"
           />
 
-          <div className="d-flex justify-content-center ">
-            <button className="btn btn-outline-success btn-sm">
-              เพิ่มขนส่ง
-            </button>
+          <div className="d-flex justify-content-center">
+            <Button onClick={() => navigate('/express')}>ยกเลิก</Button>
             &nbsp;&nbsp;&nbsp;
-            <button className="btn btn-sm" onClick={() => navigate('/express')}>
-              ยกเลิก
-            </button>
+            <Button variant="contained" color="primary" type="submit">
+              เพิ่มขนส่ง
+            </Button>
           </div>
         </form>
       </div>
--- src/views/express/ExpressList.jsx
+++ src/views/express/ExpressList.jsx
@@ -28,12 +28,12 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 export default function ExpressList() {
   let [data, setData] = useState('')
-  const form = useRef()
-  const exname = useRef()
-  const fprice = useRef()
-  const sprice = useRef()
-  const maxprice = useRef()
-  const whenfprice = useRef()
+  const form = useRef(null)
+  const exname = useRef(null)
+  const fprice = useRef(null)
+  // const sprice = useRef()
+  // const maxprice = useRef()
+  // const whenfprice = useRef()
   const date_start = useRef()
   const token = localStorage.getItem('token')
   let [selectedId, setSelectedId] = useState('')
@@ -56,161 +56,6 @@ export default function ExpressList() {
       .catch((e) => alert(e))
   }, [selectedId])
 
-  const showData = (data) => {
-    let updateForm = (
-      <div className="position-relative m-3">
-        <div className="d-flex justify-content-end mb-2">
-          <Link to="/express/delete">
-            <Button variant="contained" color="error">
-              ลบขนส่งออก
-            </Button>
-          </Link>
-
-          <Link to="/express/create" className="ms-3">
-            <Button variant="contained" color="primary">
-              เพิ่มขนส่งใหม่
-            </Button>
-          </Link>
-        </div>
-        <form onSubmit={onSubmitForm} ref={form}>
-          <TableContainer component={Paper}>
-            <Table>
-              <caption className="ms-3">
-                <small>
-                  เลือกรายการที่จะแก้ไข แล้วใส่ข้อมูลใหม่ลงไป จากนั้นคลิกปุ่ม
-                  แก้ไข
-                </small>
-              </caption>
-              <TableHead>
-                <TableRow>
-                  <TableCell>
-                    <MdGrid3X3 />
-                  </TableCell>
-                  <TableCell>
-                    <strong>ชื่อขนส่ง</strong>
-                  </TableCell>
-                  <TableCell>
-                    <strong>ค่าส่งเริ่มต้น</strong>
-                  </TableCell>
-                  <TableCell>
-                    <strong>ค่าส่งชิ้นต่อไป</strong>
-                  </TableCell>
-                  <TableCell>
-                    <strong>ค่าส่งสูงสุด</strong>
-                  </TableCell>
-                  <TableCell>
-                    <strong>ส่งฟรีต่อเมื่อยอดถึง</strong>
-                  </TableCell>
-                  <TableCell>
-                    <strong>วันที่เริ่มใช้</strong>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {data.map((doc) => {
-                  let dt = new Date(Date.parse(doc.date_start))
-                  let df = (
-                    <>
-                      {dt.getDate()}-{dt.getMonth() + 1}-{dt.getFullYear()}
-                    </>
-                  )
-
-                  return (
-                    <StyledTableRow key={doc._id}>
-                      {/* เมื่อคลิก radio บนรายการใด เราก็แนบ document ของรายการนั้น
-                          ไปยังฟังก์ชันเป้าหมาย เพื่อใช้ในการอ่านข้อมูลจากแต่ละฟิลด์ไปแสดงที่ฟอร์ม
-                      */}
-                      <TableCell>
-                        <Radio
-                          color="primary"
-                          checked={selectedId == doc._id}
-                          onChange={() => onClickRadio(doc)}
-                          value={doc._id}
-                        />
-                      </TableCell>
-
-                      <TableCell>{doc.exname}</TableCell>
-                      <TableCell>{doc.fprice}</TableCell>
-                      <TableCell>{doc.sprice}</TableCell>
-                      <TableCell>{doc.maxprice}</TableCell>
-                      <TableCell>{doc.whenfprice}</TableCell>
-                      <TableCell>{df}</TableCell>
-                    </StyledTableRow>
-                  )
-                })}
-
-                {/* สร้างฟอร์มไว้ที่แถวสุดท้าย */}
-                <TableRow>
-                  <TableCell>
-                    <Button type="submit" variant="contained" color="warning">
-                      แก้ไข
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="text"
-                      name="exname"
-                      placeholder="ชื่อขนส่ง "
-                      ref={exname}
-                      className="form-control form-control-sm"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="text"
-                      name="fprice"
-                      placeholder="ค่าส่งเริ่มต้น"
-                      ref={fprice}
-                      className="form-control form-control-sm"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="number"
-                      name="sprice"
-                      placeholder="ค่าส่งชิ้นต่อไป"
-                      ref={sprice}
-                      className="form-control form-control-sm"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="number"
-                      name="maxprice"
-                      placeholder="ค่าส่งสูงสุด"
-                      ref={maxprice}
-                      className="form-control form-control-sm"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="number"
-                      name="whenfprice"
-                      placeholder="ส่งฟรีเมื่อยอดถึง"
-                      ref={whenfprice}
-                      className="form-control form-control-sm"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="date"
-                      name="date_start"
-                      ref={date_start}
-                      className="form-control form-control-sm"
-                    />
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </form>
-      </div>
-    )
-
-    setData(updateForm)
-  }
-
   //เมื่อ radio บนรายการใดถูกคลิก (ในที่นี้เลือกใช้ click แทน change)
   //ก็อ่านข้อมูลในแต่ละฟิลต์จาก document ที่ผ่านเข้ามา แล้วเติมลงในฟอร์ม
   const onClickRadio = (doc) => {
@@ -218,10 +63,10 @@ export default function ExpressList() {
 
     exname.current.value = doc.exname
     fprice.current.value = doc.fprice
-    sprice.current.value = doc.sprice
-    maxprice.current.value = doc.maxprice
-    whenfprice.current.value = doc.whenfprice
-    // date_start.current.value = doc.date_start
+    // sprice.current.value = doc.sprice
+    // maxprice.current.value = doc.maxprice
+    // whenfprice.current.value = doc.whenfprice
+    date_start.current.value = doc.date_start
 
     let dt = new Date(Date.parse(doc.date_start))
     let y = dt.getFullYear()
@@ -232,6 +77,17 @@ export default function ExpressList() {
     let d = dt.getDate()
     d = d >= 10 ? d : '0' + d
     date_start.current.value = `${y}-${m}-${d}`
+  }
+
+  const onClickStatus = (doc) => {
+    fetch(`${baseURL}/api/ex/select`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(doc),
+    })
+      .then((res) => res.json())
+      .then((data) => showData(data))
+      .catch((error) => toast.error('Changed Status Error'))
   }
 
   const onSubmitForm = (event) => {
@@ -263,6 +119,190 @@ export default function ExpressList() {
         }
       })
       .catch((err) => toast.warning('แก้ไขข้อมูลไม่สำเร็จ'))
+  }
+
+  const showData = (data) => {
+    let updateForm = (
+      <div className="position-relative m-3">
+        {selectedId && (
+          <div className="d-flex justify-content-end mb-2">
+            <Link to="/express/delete">
+              <Button variant="contained" color="error">
+                ลบขนส่งออก
+              </Button>
+            </Link>
+
+            <Link to="/express/create" className="ms-3">
+              <Button variant="contained" color="primary">
+                เพิ่มขนส่งใหม่
+              </Button>
+            </Link>
+          </div>
+        )}
+        <form onSubmit={onSubmitForm} ref={form}>
+          <TableContainer component={Paper}>
+            <Table>
+              <caption className="ms-3">
+                <small>
+                  เลือกรายการที่จะแก้ไข แล้วใส่ข้อมูลใหม่ลงไป จากนั้นคลิกปุ่ม
+                  แก้ไข
+                </small>
+              </caption>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <MdGrid3X3 />
+                  </TableCell>
+                  <TableCell>
+                    <strong>ชื่อขนส่ง</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>ค่าส่งเริ่มต้น</strong>
+                  </TableCell>
+                  <TableCell className="text-center">
+                    <strong>สถานะ</strong>
+                  </TableCell>
+                  {/* <TableCell>
+                    <strong>ค่าส่งชิ้นต่อไป</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>ค่าส่งสูงสุด</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>ส่งฟรีต่อเมื่อยอดถึง</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>วันที่เริ่มใช้</strong>
+                  </TableCell> */}
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {data.map((doc) => {
+                  let dt = new Date(Date.parse(doc.date_start))
+                  let df = (
+                    <>
+                      {dt.getDate()}-{dt.getMonth() + 1}-{dt.getFullYear()}
+                    </>
+                  )
+
+                  return (
+                    <StyledTableRow key={doc._id}>
+                      {/* เมื่อคลิก radio บนรายการใด เราก็แนบ document ของรายการนั้น
+                          ไปยังฟังก์ชันเป้าหมาย เพื่อใช้ในการอ่านข้อมูลจากแต่ละฟิลด์ไปแสดงที่ฟอร์ม
+                      */}
+                      <TableCell sx={{ width: '150px' }}>
+                        <Radio
+                          color="primary"
+                          checked={selectedId === doc._id}
+                          onChange={() => onClickRadio(doc)}
+                          value={doc._id}
+                        />
+                      </TableCell>
+
+                      <TableCell>{doc.exname}</TableCell>
+                      <TableCell>{doc.fprice}</TableCell>
+                      {/* <TableCell>{doc.sprice}</TableCell>
+                      <TableCell>{doc.maxprice}</TableCell>
+                      <TableCell>{doc.whenfprice}</TableCell>
+                      <TableCell>{df}</TableCell> */}
+                      <TableCell className="text-center">
+                        {doc.isSelectEx ? (
+                          <Button
+                            variant="contained"
+                            color="success"
+                            onClick={() => onClickStatus(doc)}
+                          >
+                            กำลังใช้งาน
+                          </Button>
+                        ) : (
+                          <Button
+                            variant="outline"
+                            color="inherit"
+                            onClick={() => onClickStatus(doc)}
+                          >
+                            ไม่ได้ใช้งาน
+                          </Button>
+                        )}
+                      </TableCell>
+                    </StyledTableRow>
+                  )
+                })}
+
+                {/* สร้างฟอร์มไว้ที่แถวสุดท้าย */}
+                {selectedId && (
+                  <TableRow>
+                    <TableCell>
+                      <Button type="submit" variant="contained" color="warning">
+                        แก้ไข
+                      </Button>
+                    </TableCell>
+
+                    <TableCell>
+                      <input
+                        type="text"
+                        name="exname"
+                        placeholder="ชื่อขนส่ง"
+                        ref={exname}
+                        className="form-control form-control-sm"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <input
+                        type="text"
+                        name="fprice"
+                        placeholder="ค่าส่งเริ่มต้น"
+                        ref={fprice}
+                        className="form-control form-control-sm"
+                      />
+                    </TableCell>
+
+                    {/* <TableCell>
+                    <input
+                      type="number"
+                      name="sprice"
+                      placeholder="ค่าส่งชิ้นต่อไป"
+                      ref={sprice}
+                      className="form-control form-control-sm"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      type="number"
+                      name="maxprice"
+                      placeholder="ค่าส่งสูงสุด"
+                      ref={maxprice}
+                      className="form-control form-control-sm"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      type="number"
+                      name="whenfprice"
+                      placeholder="ส่งฟรีเมื่อยอดถึง"
+                      ref={whenfprice}
+                      className="form-control form-control-sm"
+                    />
+                  </TableCell> */}
+
+                    <TableCell>
+                      <input
+                        type="date"
+                        name="date_start"
+                        ref={date_start}
+                        className="form-control form-control-sm"
+                      />
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </form>
+      </div>
+    )
+
+    setData(updateForm)
   }
 
   return (
